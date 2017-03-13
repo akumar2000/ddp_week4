dee26393505eea84919220e48924bb0bf5cfafca
jensschum <contact@fluid-domains.com>
2017-03-13 15:09:30 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ddp_week4
+# Developing Data Products: Week 4 Assignment
+
+This repository contains the files for the shiny app (server.R and ui.R) and the rpubs documentation (presentation.Rpres)
---
+++ ui.R
@@ -0,0 +1,32 @@
+library(shiny)
+
+# Define UI for miles per gallon application
+shinyUI(pageWithSidebar(
+  
+  # Application title
+  headerPanel("Linear Regression Application"),
+  
+  # Sidebar with controls to select the variable to plot against mpg
+  # and to specify whether outliers should be included
+  sidebarPanel( 
+    h4("1. Select Variable to conduct linear regression analyses for the predicted variable mpg."),
+    h4("2. The fitted curve ('selected variable' vs. mpg) is plotted on the right hand side and the equation for the linear regression is displayed"),
+    h4("3. The r-squared value and he residual plot are provided to select the most suitable variable for the linear regression"),
+    selectInput("select", "Variable:",
+                list("Horse Power" = "hp", 
+                     "Weight" = "wt", 
+                     "Cylinders" = "cyl", 
+                     "Displacement" = "disp", 
+                     "Gears" = "gear",
+                     "1/4 mile time" = "qsec"))
+  ),
+  
+  # Show the caption and plot of the requested variable against mpg
+  mainPanel(
+    plotOutput("plot"),
+    h4(textOutput("equation")),
+    h4(textOutput("rsquared")),
+    plotOutput("plot_resid")
+  )
+))
+
